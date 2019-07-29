b95c56ea896b32dda451e885861486eae2822755
Bogdan Lazar <bogdan@bogdanlazar.com>
2019-07-29 15:41:56 +0200

    Improve colors and color contrast

--- src/config/edgeSerene.js
+++ src/config/edgeSerene.js
@@ -2,32 +2,32 @@ export default {
   colors: {
     backgrounds: {
       background: '#fdf6e3',
-      foreground: '#805330',
+      foreground: '#633f24',
       highlight: '#eae1cb',
       selection: '#ddc990'
     },
     text: {
-      default: '#805330',
-      caret: '#805330',
-      link: '#da2731',
+      default: '#633f24',
+      caret: '#633f24',
+      link: '#c41922',
       linkHover: '#097c8e',
       contrast: {
-        light: '#849292',
-        high: '#805330'
+        light: '#707c7c',
+        high: '#633f24'
       },
       accents: {
-        amber: '#9e6506',
+        amber: '#a95e08',
         blue: '#4b70b8',
-        brown: '#805330',
+        brown: '#633f24',
         cyan: '#097c8e',
         green: '#2e7820',
         grey: '#637479',
         indigo: '#8745ff',
         lime: '#277f65',
-        orange: '#ad5c00',
+        orange: '#a95e08',
         pink: '#d91b67',
         purple: '#562ec2',
-        red: '#da2731',
+        red: '#c41922',
         teal: '#187d86',
         yellow: '#9e6400',
         white: '#757169'
@@ -37,7 +37,7 @@ export default {
       background: '#fdf6e3',
       default: {
         background: '#fdf6e3',
-        text: '#805330'
+        text: '#633f24'
       },
       active: {
         background: '#f2ead5',
@@ -46,7 +46,7 @@ export default {
     },
     borders: {
       color: '#ddc990',
-      focus: '#849292'
+      focus: '#707c7c'
     },
     scope: {
       comment: '#717171',
@@ -55,19 +55,21 @@ export default {
       constant: '#ad5c00',
       constructor: '#8745ff',
       variable: '#637479',
-      keyword: '#da2731',
+      keyword: '#c41922',
+      userVar: '#ad5c00',
       storage: '#d91b67',
       storageType: '#4b70b8',
       className: '#2e7820',
       functionName: '#097c8e',
       functionArg: '#2e7820',
-      tagName: '#da2731',
+      jsProp: '#138232',
+      tagName: '#c41922',
       tagAttr: '#277f65',
       libraryFn: '#097c8e',
       libraryConstant: '#ad5c00',
       libraryClass: '#2e7820',
       libraryVariable: '#637479',
-      cssDefault: '#805330',
+      cssDefault: '#633f24',
       cssProp: '#d91b67',
       cssNumber: '#ad5c00',
       cssUnit: '#ad5c00',
@@ -77,7 +79,7 @@ export default {
       jsBrace: '#637479',
       jsonLevel0: '#4b70b8',
       jsonLevel1: '#9e6506',
-      jsonLevel2: '#805330',
+      jsonLevel2: '#633f24',
       jsonLevel3: '#097c8e',
       jsonLevel4: '#2e7820',
       jsonLevel5: '#637479',
@@ -88,7 +90,7 @@ export default {
       gitgutterUntracked: '#9e6400',
       gitgutterInserted: '#2e7820',
       gitgutterChanged: '#ad5c00',
-      gitgutterDeleted: '#da2731'
+      gitgutterDeleted: '#c41922'
     }
   }
 };
